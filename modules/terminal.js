081b638aea9b60fa517e15fc651abfe59561f736
Reansykes <85455939+reansky@users.noreply.github.com>
2026-07-18 17:32:26 +0700

    Update terminal.js

--- modules/terminal.js
+++ modules/terminal.js
@@ -1,1 +1,17 @@
+/* =====================================================
+   THE CLOAK TERMINAL
+===================================================== */
 
+window.Terminal = {
+
+    version: "1.0.0",
+
+    start() {
+
+        console.log("🟢 The Cloak Terminal Loaded");
+
+    }
+
+};
+
+Terminal.start();
